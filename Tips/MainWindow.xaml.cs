e6bab9e4cec634a62f5b52ec3395fb39099180dd
Dainter <xiaogang.dai@siemens.com>
2016-12-30 11:36:25 +0800

    2016-12-30:        1.图标加载        2.毛玻璃界面        3.通知栏图标

--- Tips/MainWindow.xaml.cs
+++ Tips/MainWindow.xaml.cs
@@ -9,20 +9,89 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Interop;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
 
 namespace Tips
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : GWindow
     {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MARGINS
+        {
+            public int cxLeftWidth;
+            public int cxRightWidth;
+            public int cyTopHeight;
+            public int cyBottomHeight;
+        };
+
+        [DllImport("DwmApi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(
+            IntPtr hwnd,
+            ref MARGINS pMarInset);
+        //Global Elements
+
+
         public MainWindow()
         {
             InitializeComponent();
+            FrmMainInit();
         }
+
+        private void ExtendAeroGlass(Window window)
+        {
+            try
+            {
+                // 为WPF程序获取窗口句柄
+                IntPtr mainWindowPtr = new WindowInteropHelper(window).Handle;
+                HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+                mainWindowSrc.CompositionTarget.BackgroundColor = Colors.Transparent;
+
+                // 设置Margins
+                MARGINS margins = new MARGINS();
+
+                // 扩展Aero Glass
+                margins.cxLeftWidth = -1;
+                margins.cxRightWidth = -1;
+                margins.cyTopHeight = -1;
+                margins.cyBottomHeight = -1;
+
+                int hr = DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+                if (hr < 0)
+                {
+                    MessageBox.Show("DwmExtendFrameIntoClientArea Failed");
+                }
+            }
+            catch (DllNotFoundException)
+            {
+                Application.Current.MainWindow.Background = Brushes.White;
+            }
+        }
+
+        private void FrmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Background = Brushes.Transparent;
+            ExtendAeroGlass(this);
+        }
+
+        private void FrmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void FrmMainInit()
+        {
+
+            
+        }
+
     }
 }
